23441fbfd36d3dff7c685ab2eba76c3a20f981d6
rossedfort <rossedfort@gmail.com>
2015-10-19 19:29:52 -0600

    using active record

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,5 +1,11 @@
 class TaskManager < Sinatra::Base
   get '/tasks' do
+    @tasks = Task.all
     erb :tasks_index
+  end
+
+  get '/users' do
+    @users = User.all
+    erb :users_index
   end
 end
